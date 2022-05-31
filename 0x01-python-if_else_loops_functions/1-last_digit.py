ea0a0b23d114d177ab9624a058813fd7a3636d53
laurencia behanzin <laurenciabehanzin22@gmail.com>
2022-05-31 17:07:07 +0100

    last digit

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number > 0:
+    modulo = number % 10
+else:
+    modulo = number % -10
+if modulo > 5:
+    print("Last digit of {:d} is {:d} and is greater than 5".format(number, modulo))
+elif modulo == 0:
+    print("Last digit of {:d} is {:d} and is 0".format(number, modulo))
+else:
+    print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, modulo))
