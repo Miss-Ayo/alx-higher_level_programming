45d691f0d9da675ba0f5a8c593ac3727888c1c61
laurencia behanzin <laurenciabehanzin22@gmail.com>
2022-06-01 03:33:00 +0100

    add py

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def add(a, b):
-    if a < 0 and a > b:
-        return (b - a)
-    elif b < 0 and b > a:
-        return (a - b)
-    else:
-        return (a + b)
+    return (a + b)
