eeb7e1a6262fd6599f73c24a66a1de1f8a1c7e46
laurencia behanzin <laurenciabehanzin22@gmail.com>
2022-06-02 17:20:07 +0100

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,10 +2,10 @@
 if __name__ == "__main__":
     from sys import argv
     j = len(argv)
-    if (j == 2):
+    if j == 2:
         print("{:d} argument:".format(j - 1))
     else:
-        if (j == 1):
+        if j == 1:
             print("{:d} arguments.".format(j - 1))
         else:
              print("{:d} arguments:".format(j - 1))
