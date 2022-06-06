20fa5ae3a3324e0358622f256daca61dc8aa00ed
laurencia behanzin <laurenciabehanzin22@gmail.com>
2022-06-06 23:05:51 +0100

    add tuple

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = ""
-    for i in my_string:
-        if i is not 'c' and i is not 'C':
-            new_string += i
-    return new_string
+    return(''.join(c for c in my_string if c not in 'cC'))
