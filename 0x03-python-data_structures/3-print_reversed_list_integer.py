f1d7cf0666dca8cd4747dc049655162deeaef393
laurencia behanzin <laurenciabehanzin22@gmail.com>
2022-06-06 15:17:17 +0100

    reversed list

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from audioop import reverse
+
+
+def print_reversed_list_integer(my_list=[]):
+    for i in reversed(my_list):
+        print("{:d}".format(i))
+
+my_list = [1, 2, 3, 4, 5]
+print_reversed_list_integer(my_list)
